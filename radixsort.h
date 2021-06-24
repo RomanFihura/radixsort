70f534cc41ba5c8d92d8b207f78fc8895fe75a61
Roman Fihura <romanfigura120@gmail.com>
2021-06-24 22:47:27 +0300

    updates with improves

--- radixsort.h
+++ radixsort.h
@@ -1,8 +1,6 @@
 #pragma once
 
+#include <algorithm>
 #include <stdint.h>
 #include <vector>
-
-uint32_t getMax(std::vector<uint32_t> v);
-void countSort(std::vector<uint32_t> &v, uint32_t exp);
 void radixsort(std::vector<uint32_t> &v);
